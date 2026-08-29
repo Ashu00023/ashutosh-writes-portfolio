0a21fdd35199446b7c14c9216e1338c225badf3a
Ashu00023 <ashutosh.mahapatra2398@gmail.com>
2026-08-29 11:59:29 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,6 @@ const links = [
   { label: "Home", href: "#home" },
   { label: "About", href: "#about" },
   { label: "Services", href: "#services" },
-  { label: "Portfolio", href: "#portfolio" },
   { label: "Contact", href: "#contact" },
 ];
 
