1bdcd56eb41ab2a5c2fc71194d3c9a6e87d62a1a
Ashu00023 <ashutosh.mahapatra2398@gmail.com>
2026-09-19 16:45:43 +0530

    Update HumanCreativityVsAi.tsx

--- src/pages/blog-posts/HumanCreativityVsAi.tsx
+++ src/pages/blog-posts/HumanCreativityVsAi.tsx
@@ -40,7 +40,7 @@ const HumanCreativityVsAi = () => {
         <StaticHtmlPage
           rawHtml={rawHtml}
           rightLabel="SEO Blog Sample · Authenticity Premium"
-          background="#080808"
+          background="#faf7f1"
           canonicalUrl={url}
           breadcrumbs={[
             { label: "Home", href: "/" },
